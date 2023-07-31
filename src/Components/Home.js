0b53576bbdb51e9d571f7694552e53c07bfba9fb
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-07-31 15:59:23 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,11 +8,15 @@ import ClientsSection from './ClientsSection';
 import ShipRates from './ShipRates';
 import { Link } from 'react-router-dom';
 import Line from './Line';
+import './Home.css'; // Import a custom CSS file for Home component styles
+
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <HeroSection />
+      <div className="small-gap"></div>
       <StatsCard />
+      <div className="small-gap"></div>
       <ClientsSection />
       <Footer />
     </div>
