02ecfb719d4be9f2d500d9337b7a10fe30b072b9
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-08-01 20:52:08 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,15 +12,37 @@ const About = () => {
       <div className="mission-vision-value-container">
         <div className="mission">
           <h2>MISSION</h2>
-          <p>Empowering institutions, businesses, and students with cutting-edge logistics and packers & movers solutions, Campus Express is committed to revolutionizing the industry. Our mission is to simplify the complexities of transportation and supply chain management, while providing unparalleled service quality and customer satisfaction. Through our innovative approach and global reach, we strive to create a seamless experience for all our clients, delivering excellence in every aspect of our operations.</p>
+  <ul>
+    <li>Empowering institutions, businesses, and students with cutting-edge logistics and packers &amp; movers solutions.</li>
+    <li>A commitment to revolutionizing the industry.</li>
+    <li>A mission to simplify the complexities of transportation and supply chain management.</li>
+    <li>Unparalleled service quality and customer satisfaction.</li>
+    <li>An innovative approach and global reach to create a seamless experience for all clients.</li>
+    <li>Excellence in every aspect of our operations.</li>
+  </ul>
+
         </div>
         <div className="vision">
           <h2>VISION</h2>
-          <p>At Campus Express, we envision a world where logistics and packers & movers services are synonymous with efficiency and reliability. Our vision is to be the foremost choice for institutions, companies, and students seeking hassle-free, tailored logistics solutions. By leveraging technology and our team's expertise, we aim to set new industry standards, continually expanding our global presence and enhancing the lives of our customers.</p>
+          <ul>
+  <li>At Campus Express, we envision a world where logistics and packers &amp; movers services are synonymous with efficiency and reliability.</li>
+  <li>Our vision is to be the foremost choice for institutions, companies, and students seeking hassle-free, tailored logistics solutions.</li>
+  <li>By leveraging technology and our team's expertise, we aim to set new industry standards.</li>
+  <li>We are committed to continually expanding our global presence.</li>
+  <li>We strive to enhance the lives of our customers.</li>
+</ul>
+
         </div>
         <div className="value">
           <h2>VALUE</h2>
-          <p>Campus Express is a 3-year-old Logistics and Packers & Movers company. We help Institutions, Companies & Universities simplify their logistics challenges. Till date, we have successfully served several domestic & international clients alongside serving more than 15,000+ students from 300+ colleges, with a record success rate. We are spread across companies, HEIs, and extend our service for both domestic & international logistics alongside custom consultation. We simplify logistics to make your experience friction-free.</p>
+          <ul>
+  <li>Campus Express is a 3-year-old Logistics and Packers &amp; Movers company.</li>
+  <li>We help Institutions, Companies &amp; Universities simplify their logistics challenges.</li>
+  <li>Till date, we have successfully served several domestic &amp; international clients.</li>
+  <li>We have served more than 15,000+ students from 300+ colleges with a record success rate.</li>
+  <li>We are spread across companies, HEIs (Higher Education Institutions), and extend our service for both domestic &amp; international logistics alongside custom consultation.</li>
+</ul>
+
         </div>
       </div>
     </div>
